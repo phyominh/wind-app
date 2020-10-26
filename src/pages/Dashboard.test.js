bcfc6c717c10ca32da9308a2b01511f128a06272
phyominh <23469622+phyominh@users.noreply.github.com>
2020-10-26 22:29:30 +0630

    ⛔ Removed flawed tests in Dashboard, these tests will be reimplemented in future integration tests

--- src/pages/Dashboard.test.js
+++ src/pages/Dashboard.test.js
@@ -1,81 +1,14 @@
 import React from "react";
-import { act } from "react-dom/test-utils";
-import { Provider } from "react-redux";
-import { addSeconds, format, getSeconds } from "date-fns";
-import { mount } from "enzyme";
-import configureStore from "redux-mock-store";
+import { shallow } from "enzyme";
 
 import Dashboard from "./Dashboard.js";
+import StatusBar from "../components/StatusBar.js";
+import WindData from "../components/WindData.js";
 
-describe("Dashboard", () => {
-  const mockStore = configureStore();
-  const timeFormat = "h:mm a";
-
-  let dashboard, store, testDate;
-
-  beforeEach(() => {
-    store = mockStore({});
-    dashboard = mount(
-      <Provider store={store}>
-        <Dashboard />
-      </Provider>
-    );
-    testDate = new Date();
-    jest.useFakeTimers("modern");
-  });
-
-  afterEach(() => {
-    dashboard.unmount();
-    jest.clearAllTimers();
-  });
-
-  describe("Passing data to StatusBar", () => {
-    test("renders formatted time in StatusBar", () => {
-      expect(
-        dashboard
-          .find(".status-bar .column.is-3")
-          .at(0)
-          .find(".status-value")
-          .text()
-      ).toEqual(format(testDate, timeFormat));
-    });
-
-    test("renders formatted time only if minute or hour changes", () => {
-      if (getSeconds(testDate) == 59) {
-        act(() => jest.advanceTimersByTime(1000));
-        addSeconds(testDate, 1);
-      }
-
-      // since the clock in StatusBar updates at a minute interval increasing a second to the timer should not update the component
-      act(() => jest.advanceTimersByTime(1000));
-
-      const renderedTime = dashboard
-        .find(".status-bar .column.is-3")
-        .at(0)
-        .find(".status-value")
-        .text();
-      expect(renderedTime).toEqual(format(testDate, timeFormat));
-
-      // 1 minute added and the component should now be updated
-      act(() => jest.advanceTimersByTime(60000));
-
-      const renderedTimeAfterOneMin = dashboard
-        .find(".status-bar .column.is-3")
-        .at(0)
-        .find(".status-value")
-        .text();
-      expect(renderedTimeAfterOneMin).not.toEqual(renderedTime);
-
-      // add 1 hour
-      act(() => jest.advanceTimersByTime(3600000));
-
-      const renderedTimeAfterOneHour = dashboard
-        .find(".status-bar .column.is-3")
-        .at(0)
-        .find(".status-value")
-        .text();
-      expect(renderedTimeAfterOneHour).not.toEqual(renderedTime);
-      expect(renderedTimeAfterOneHour).not.toEqual(renderedTimeAfterOneMin);
-    });
+describe("📈 Dashboard", () => {
+  test("renders child components", () => {
+    const dashboard = shallow(<Dashboard />);
+    expect(dashboard.find(StatusBar)).toHaveLength(1);
+    expect(dashboard.find(WindData)).toHaveLength(1);
   });
 });
